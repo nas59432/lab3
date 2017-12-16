a26cfdc97ac25d526bcbc2833784970618152710
Raynir <Dmitriy.petrov99@gmail.com>
2017-12-16 14:13:35 +0700

    Отступы добавлены, ошибки исправлены

--- ConsoleApplication11.cpp
+++ ConsoleApplication11.cpp
@@ -9,13 +9,17 @@ using namespace std;
 void input(int* a, int* b, int* c)
 {
 	cout << "Найдем максимальное значение из трех чисел " << endl;
+
 	cout << "Введите первое число" << endl;
 	cin >> *a;
+
 	cout << "Введите второе число" << endl;
 	cin >> *b;
+
 	cout << "Введите третье число" << endl;
 	cin >> *c;
 }
+
 int solution(int a, int b, int c, int max)
 {
 
@@ -23,22 +27,28 @@ int solution(int a, int b, int c, int max)
 	{
 		max = a;
 	}
+
 	if ((b >= a) && (b >= c))
 	{
 		max = b;
 	}
+
 	if ((c >= b) && (c >= a))
 	{
 		max = c;
 	}
+
 	return max;
 }
+
 void output(int max)
 {
 	cout << "Наибольшее число = " << max << endl;
+
 	getchar();
 	getchar();
 }
+
 int main()
 {
 	setlocale(LC_ALL, "rus");
@@ -46,7 +56,6 @@ int main()
 	input(&a, &b, &c);
 	max = solution(a, b, c, max);
 	output(max);
-
 
 	return 0;
 }
